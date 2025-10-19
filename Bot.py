3048cc5d3f1bd5dee4914daa687fe739b8ff6671
Israel-jv <143968932+Israel-jv@users.noreply.github.com>
2025-10-19 13:53:19 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-BOT_TOKEN = "8289064969:AAEyirLo6aM14HJYw2vgFHfkQP_T7kUubg8"
+BOT_TOKEN = "BOT_TOKEN"
 FORWARD_CHAT_ID = 6335294113  # your Telegram user ID
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -27,3 +27,4 @@ if __name__ == "__main__":
 
     print("✅ Bot is running...")
     app.run_polling()
+
